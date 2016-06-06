a646d08768634a45c2492bf6d354689f0584382a
jhoselyn Inga <inga.jhoselyn@gmail.com>
2016-06-06 15:03:10 -0500

    final

--- assets/js/cbpAnimatedHeader.js
+++ assets/js/cbpAnimatedHeader.js
@@ -3,13 +3,13 @@ var cbpAnimatedHeader = (function() {
 	var docElem = document.documentElement,
 		header = document.querySelector( '.navbar-default' ),
 		didScroll = false,
-		changeHeaderOn = 300;
+		changeHeaderOn = 20;
 
 	function init() {
 		window.addEventListener( 'scroll', function( event ) {
 			if( !didScroll ) {
 				didScroll = true;
-				setTimeout( scrollPage, 500 );
+				setTimeout( scrollPage, 50 );
 			}
 		}, false );
 	}
